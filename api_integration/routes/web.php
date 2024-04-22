729b2f4ab60432e450f874bfcfea3d15c93830b4
Noufal <noufal@n.com>
2024-04-22 13:07:26 +0530

    Update employee route

--- api_integration/routes/web.php
+++ api_integration/routes/web.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[EmployeeController::class, 'index'])->name('employee.index');
+// Route::get('/',[EmployeeController::class, 'index'])->name('employee.index');
+// Route::get('/employee/{employee}',[EmployeeController::class, 'show'])->name('employee.show');
+Route::resource('employee',EmployeeController::class);
